8408be3df55f1cb7cd11f9f94fd7bfeda63b2152
Dibyajyoti10 <156401898+Dibyajyoti10@users.noreply.github.com>
2025-10-13 16:34:31 +0530

    New project started

---
+++ src/models/name.ts
@@ -0,0 +1,8 @@
+// keep all the names to be standardized to use them wherever we want
+
+export const db = "main-stack"
+export const questionCollection = "questions"
+export const answerCollection = "answers"
+export const commentCollection = "comments"
+export const questionAttachment = "question-attachments"
+export const voteCollection = "votes"
---
+++ src/models/server/dbSetup.ts
@@ -0,0 +1,1 @@
+// just for seeding / initializing the database
